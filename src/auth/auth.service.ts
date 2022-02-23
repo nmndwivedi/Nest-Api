e971b09e4d9c025b3d9bc005e31d0bbf063c0c47
Naman <nmndwivedi@gmail.com>
2022-02-23 02:36:04 +0100

    Login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { AuthDTO } from './dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { LoginDTO, SignupDTO } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
-  login(body: AuthDTO) {
-    return { body };
+  constructor(private prisma: PrismaService) {}
+
+  async login(credentials: LoginDTO) {
+    let user = await this.prisma.user.findFirst({where: {email: credentials.email}});
+    console.log(user);
+    return { credentials };
+  }
+
+  async signup(d: SignupDTO) {
+    let pHash = await argon.hash(d.password);
+    let user = await this.prisma.user.create({
+      data: {
+        firstName: d.firstName,
+        lastName: d.lastName,
+        email: d.email,
+        passwordHash: pHash
+      }
+    });
+
+    return user;
   }
 }
---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+
+export class SignupDTO {
+    @IsString()
+    @IsNotEmpty()
+    firstName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    lastName: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+}
